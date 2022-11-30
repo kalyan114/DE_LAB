7627585d4d8b749f8dfdecc7501e06e44690fdaa
Kalyan Kumar Thanguturi <81627943+kalyan114@users.noreply.github.com>
2022-11-30 10:53:35 +0530

    Update smoothing.c

--- smoothing.c
+++ smoothing.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<math.h>
 int main()
 {
-    int n=27,d=3,i=0,j=0,c=1,s=0,b[n];
-    int a[27]={13,15,16,16,19,20,20,21,22,22,25,25,25,25,30,33,33,35,35,35,35,36,40,45,46,52,70};
-    while(c<=d){
-        for(i=j;i<j+(n/d);i++){s+=a[i];}
-        s/=(n/d);
-        for(i=j;i<j+(n/d);i++){b[i]=s;}
-        j=i;s=0;c++;
-    }
-    for(int i=0;i<n;i++){printf("%d ",b[i]);}
-    return 0;
+        int n=27,i=0,j=0,s=0,c=1,d=3,b[27];
+        for(int t=0;t<27;t++){b[t]=0;}
+        int a[27]={13,15,16,16,19,20,20,21,22,22,25,25,25,25,30,33,33,35,35,35,35,36,40,45,46,52,70};
+        while(c<=d){
+                for(i=j;i<j+(n/d);i++){s+=a[i];}
+                s/=(n/d);
+                for(i=j;i<j+(n/d);i++){b[i]=s;printf("%d ",b[i]);}
+                j=i;s=0;c++;
+        }
+        printf("\n");
+        return 0;
 }
 
 
